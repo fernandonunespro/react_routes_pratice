1862826ba3a4782704e4cf10550720b659e9d321
Fernando Nunes <nunesfernandopro@gmail.com>
2023-09-19 00:06:48 +0200

    Add Produtos

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,9 @@
+function Erro(){
+  return(
+    <div>
+      <h2>Ops, parece que a pagina nao existe</h2>
+    </div>
+  )
+};
+
+export default Erro;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,10 @@ function Home() {
       <h1>Bem vindo a Home</h1><br></br><br></br>
       <Link to="/sobre">Sobre</Link><br></br>
       <Link to="/contato">Contato</Link>
+
+      <br></br><hr></hr><br></br>
+
+      <Link to="/produto/12345">Acessar Produto 12345</Link>
     </div>
   );
 }
